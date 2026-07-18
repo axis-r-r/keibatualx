0505dee1522102f392a69b214341b5c204f2e849
axis-r-r <puonpuon5@gmail.com>
2026-07-18 20:09:46 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "virtual-keiba-rc4-0";
+const CACHE_NAME = "virtual-keiba-v1-0";
 const CORE = ["/", "/index.html", "/manifest.webmanifest", "/icon-192.png", "/icon-512.png"];
 
 self.addEventListener("install", event => {
